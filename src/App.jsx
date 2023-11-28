b94aaca08a1220dbd6c10984e7006aafeb539ef0
PowerOfMind <jubaga95@gmail.com>
2023-11-28 20:15:52 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DatePicker } from 'antd';
 
 function App() {
   const [count, setCount] = useState(0)
